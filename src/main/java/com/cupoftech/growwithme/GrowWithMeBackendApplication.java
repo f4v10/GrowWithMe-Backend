9f3c63391a9ab1a46d890c91d6aa02cc5540365b
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-08-23 23:06:51 -0500

    chore: initial commit.

---
+++ src/main/java/com/cupoftech/growwithme/GrowWithMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cupoftech.growwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowWithMeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowWithMeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cupoftech/growwithme/GrowWithMeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupoftech.growwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowWithMeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
