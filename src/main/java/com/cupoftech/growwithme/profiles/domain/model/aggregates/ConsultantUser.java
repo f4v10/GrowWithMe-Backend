27f0077e474ebc92d27361e3b86f0eaa301d3e3b
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-11-02 16:25:50 -0500

    fix: fixed aggregates ConsultantUser and FarmerUser.

--- src/main/java/com/cupoftech/growwithme/profiles/domain/model/aggregates/ConsultantUser.java
+++ src/main/java/com/cupoftech/growwithme/profiles/domain/model/aggregates/ConsultantUser.java
@@ -3,6 +3,8 @@ package com.cupoftech.growwithme.profiles.domain.model.aggregates;
 import com.cupoftech.growwithme.profiles.domain.model.commands.consultant.CreateConsultantUserCommand;
 import com.cupoftech.growwithme.profiles.domain.model.valueobjects.EmailAddress;
 import com.cupoftech.growwithme.profiles.domain.model.valueobjects.PersonName;
+import com.cupoftech.growwithme.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,33 +12,35 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class ConsultantUser extends User {
-
+public class ConsultantUser extends AuditableAbstractAggregateRoot<ConsultantUser> {
+    @Embedded
+    private PersonName name;
+    @Embedded
+    private EmailAddress email;
+    private String phone;
     private String dni;
 
     public ConsultantUser() {}
 
     public ConsultantUser(String firstName, String lastName, String email, String phone, String dni) {
-        super();
-        this.setName(new PersonName(firstName, lastName));
-        this.setEmail(new EmailAddress(email));
-        this.setPhone(phone);
+        this.name = new PersonName(firstName, lastName);
+        this.email = new EmailAddress(email);
+        this.phone = phone;
         this.dni = dni;
     }
 
     public ConsultantUser(CreateConsultantUserCommand command) {
-        super();
-        this.setName(new PersonName(command.firstName(), command.lastName()));
-        this.setEmail(new EmailAddress(command.email()));
-        this.setPhone(command.phone());
+        this.name = new PersonName(command.firstName(), command.lastName());
+        this.email = new EmailAddress(command.email());
+        this.phone = command.phone();
         this.dni = command.dni();
     }
 
     public String getFullName() {
-        return super.getFullName();
+        return name.getFullName();
     }
 
     public String getEmailAddress() {
-        return super.getEmail().email();
+        return email.email();
     }
 }
